de7d7acf57520b332503f5436d10b1419b82dca9
George Flores <georgeflores886@gmail.com>
2020-05-08 19:19:22 -0400

    created loop for time blocks

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,19 +1,29 @@
- //JavaScript function wrapping everything
- $(document).ready(function() {
-
-    var currentDateAndTime = Date(Date.now()); 
-    var timeBlocks  = $(".time-block");
+//JavaScript function wrapping everything
+$(document).ready(function () {
+  var currentDateAndTime = Date(Date.now());
+  $("#currentDay").text(currentDateAndTime);
   
-
-    $("#currentDay").text(currentDateAndTime);
-    //updating hours
-    function updateHours() {
-      var currentHour = new Date().getHours();//current military time
-        //looping through time-blocks
-      for (var i = 0; i < timeBlocks.length; i++){
-
+  //updating hours
+  function updateHours() {
+    var currentHour = new Date().getHours();//current military time
+    //looping through time-blocks
+    for (var i = 9; i < 18; i++) {
+      if (currentHour > i) {
+        $("#hour-" + i).addClass("past");
+      }
+      else if (currentHour == i) {
+        $("#hour-" + i).removeClass("past").addClass("present");
+       
+      } else {
+        $("#hour-" + i).removeClass("past").removeClass("present").addClass("future");
       }
     }
+  }
+  updateHours();
+  
+  //saving description item 
+  // $(".saveBtn").on("click", function() {
+  //   var 
+  // })
 
-
-  });
+});
