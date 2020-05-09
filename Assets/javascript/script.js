de1845fa3dc932d07d9be7d452952ce2a2b87f0c
George Flores <georgeflores886@gmail.com>
2020-05-09 11:39:28 -0400

    updated saveBtn

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,13 +1,28 @@
 //JavaScript function wrapping everything
 $(document).ready(function () {
   var currentDateAndTime = Date(Date.now());
+  var saveBtn = $(".btn");
   $("#currentDay").text(currentDateAndTime);
-
+  //save button
+  saveBtn.on("click", function() {
+    var saveHour = $(this).val();
+    var text =  $(this).siblings(".description").val().trim();
+    console.log(saveHour);
+    console.log(text);
+    localStorage.setItem(saveHour, text);
+  })
   //updating hours
   function updateHours() {
     var currentHour = new Date().getHours();//current military time
+
     //looping through time-blocks 
     for (var i = 9; i < 18; i++) {
+      //loading local storage
+      var hourText = localStorage.getItem(i);
+      if (hourText !== null) {
+        $("#hour-" + i).val(hourText);
+      }
+
       if (currentHour > i) {
         $("#hour-" + i).addClass("past");
       }
@@ -21,20 +36,7 @@ $(document).ready(function () {
   }
   updateHours();
 
-function init(hour) {
-  var h = localStorage.getItem(hour);
-
-}
-  
 //reRun updateHours every 60 seconds
 setInterval(updateHours, 60000);
-//saving description item 
-$(".saveBtn").on("click", function() {
-  var descript = $(".description");
-  var timeSaved = $("#hour-");
-  localStorage.setItem(descript, timeSaved);
-  
-})
-  
 
 });
